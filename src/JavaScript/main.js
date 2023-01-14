5e791df6703f8c7b7c33b16a912c226fcc3b533d
Daniel Lima <daniellimaa05@gmail.com>
2023-01-14 08:57:01 -0300

    style

--- src/JavaScript/main.js
+++ src/JavaScript/main.js
@@ -1,23 +1,25 @@
+(function(){
+    const raffleButton =  document.querySelector(".random-form")
+    const minNumber = document.getElementById("min-value")
+    const maxNumber = document.getElementById("max-value")
+    const randomH1 = document.querySelector(".random-num")
+    
+    
+    function randomNumber(number1, number2){
+        number1 = Math.ceil(number1);
+        number2 = Math.floor(number2);
+        const random = Math.floor(Math.random() * (number2 - number1 + 1) + number1);
+        randomH1.innerText =  `${random}`
+    }
+    
+     raffleButton.addEventListener("submit", function(event){
+     event.preventDefault()
+     const minValue = minNumber.value
+     const maxValue = maxNumber.value
+      randomNumber(minValue, maxValue)
+     })
+})();
 
-const raffleButton =  document.querySelector(".random-form")
-const minNumber = document.getElementById("min-value")
-const maxNumber = document.getElementById("max-value")
-const randomH1 = document.querySelector(".random-num")
-
-
-function randomNumber(number1, number2){
-    number1 = Math.ceil(number1);
-    number2 = Math.floor(number2);
-    const random = Math.floor(Math.random() * (number2 - number1 + 1) + number1);
-    randomH1.innerText =  `${random}`
-}
-
- raffleButton.addEventListener("submit", function(event){
- event.preventDefault()
- const minValue = minNumber.value
- const maxValue = maxNumber.value
-  randomNumber(minValue, maxValue)
- })
 
 
 
